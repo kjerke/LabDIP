21ec48cab5e0c9e5ffb26aa1744d2be66f7be123
kevinjerke <kevinjerke@172.16.42.12>
2016-09-22 14:31:57 -0500

    Added Service quality enum class

--- src/dip/lab2/student/solution1/ServiceQualityRating.java
+++ src/dip/lab2/student/solution1/ServiceQualityRating.java
@@ -1,29 +1,15 @@
+package dip.lab2.student.solution1;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dip.lab2.student.solution1;
 
 /**
  *
  * @author kevinjerke
  */
-public class ServiceQualityRating {
-    
-    
-    public static enum ServiceQuality {
-           GOOD, FAIR, POOR
-    };
-    
-    public final void setServiceRating(BaggageServiceTipCalculator.ServiceQuality q) {
-        // No need to validate because enums provide type safety!
-        BaggageServiceTipCalculator.ServiceQuality serviceQuality = q;
-    }
-
-    public BaggageServiceTipCalculator.ServiceQuality getServiceQuality() {
-        BaggageServiceTipCalculator.ServiceQuality serviceQuality = null;
-        return serviceQuality;
-   
+public enum ServiceQualityRating {
+    GOOD, FAIR, POOR
 }
-    }
